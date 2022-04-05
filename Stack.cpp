2255ead8147e546fecf68083a4d8f428c8717280
Atul Gupta <52090961+atulgupta002@users.noreply.github.com>
2022-04-05 07:34:24 +0530

    Templated list-based stack in C++.

---
+++ Stack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+template <class T>
+class Stack
+{
+    list<T> data;
+
+    public:
+    void push(T newItem){data.push_front(newItem);}
+    void pop(){return data.pop_front();}
+    T top(){return *data.begin();}
+    bool isEmpty() const {return data.size() == 0;}
+    int size() {return data.size();}
+    void clear(){data.clear();}
+};
+
+
+//Driver code
+int main()
+{
+    string str;
+    Stack<int> stk;
+    stk.push(2);
+    stk.push(5);
+    stk.push(4);
+    stk.push(9);
+    stk.push(56);
+    stk.pop();
+    cout << "Size: " << stk.size() << endl;
+}
